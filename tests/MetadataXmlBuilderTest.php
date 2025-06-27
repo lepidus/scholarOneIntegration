6b9ee1c9cdededa51d2bb9bb022abe6c375259b6
Jhon <jhon@lepidus.com.br>
2025-06-27 14:35:07 -0400

    Refactors metadata builder test class
    Issue: documentacao-e-tarefas/scielo#779
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/MetadataXmlBuilderTest.php
+++ tests/MetadataXmlBuilderTest.php
@@ -67,120 +67,7 @@ class MetadataXmlBuilderTest extends DatabaseTestCase
         }
     }
 
-    private function createExpectedXml()
-    {
-        $dom = new DOMDocument('1.0', 'UTF-8');
-        $dom->formatOutput = true;
 
-        $articleNode = $dom->createElement('article');
-        $dom->appendChild($articleNode);
-
-        $articleNode->setAttribute('xmlns:oasis', 'http://www.niso.org/standards/z39-96/ns/oasis-exchange/table');
-        $articleNode->setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
-        $articleNode->setAttribute('xmlns:mml', 'http://www.w3.org/1998/Math/MathML');
-        $articleNode->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-
-        $frontNode = $dom->createElement('front');
-        $articleNode->appendChild($frontNode);
-
-        $journalMetaNode = $this->createJournalMetaNode($dom);
-        $frontNode->appendChild($journalMetaNode);
-
-        $articleMeta = $this->createArticleMetaNode($dom);
-        $frontNode->appendChild($articleMeta);
-
-        return $dom;
-    }
-
-    private function createJournalMetaNode($dom)
-    {
-        $journalMetaNode = $dom->createElement('journal-meta');
-
-        $journalIdNode = $dom->createElement('journal-id');
-        $journalIdNode->setAttribute('journal-id-type', 'publisher');
-        $journalIdNode->appendChild($dom->createTextNode($this->clientKey));
-
-        $journalMetaNode->appendChild($journalIdNode);
-
-        $journalTitleGroupNode = $dom->createElement('journal-title-group');
-        $journalTitleNode = $dom->createElement('journal-title');
-        $journalTitleNode->appendChild($dom->createTextNode($this->journalShortName));
-        $journalTitleGroupNode->appendChild($journalTitleNode);
-
-        $journalMetaNode->appendChild($journalTitleGroupNode);
-
-        return $journalMetaNode;
-    }
-
-    private function createArticleMetaNode($dom)
-    {
-        $articleMetaNode = $dom->createElement('article-meta');
-
-        $titleGroupNode = $dom->createElement('title-group');
-        $articleTitleNode = $dom->createElement('article-title');
-        $articleTitleNode->appendChild($dom->createTextNode($this->title['pt_BR']));
-        $titleGroupNode->appendChild($articleTitleNode);
-        $articleMetaNode->appendChild($titleGroupNode);
-
-        $abstractNode = $dom->createElement('abstract');
-        $paragraph = $dom->createElement('p');
-        $paragraph->appendChild($dom->createTextNode($this->abstract['pt_BR']));
-        $abstractNode->appendChild($paragraph);
-        $articleMetaNode->appendChild($abstractNode);
-
-        $keywordsNode = $dom->createElement('kwd-group');
-        $keywordsNode->setAttribute('kwd-group-type', 'Keywords');
-        $keywordsNode->setAttribute('id', '');
-        foreach ($this->keywords['pt_BR'] as $keyword) {
-            $keywordNode = $dom->createElement('kwd');
-            $keywordNode->setAttribute('id', '');
-            $keywordNode->appendChild($dom->createTextNode($keyword));
-
-            $keywordsNode->appendChild($keywordNode);
-        }
-        $articleMetaNode->appendChild($keywordsNode);
-
-        $contributorGroupNode = $dom->createElement('contrib-group');
-        $affiliations = [];
-        foreach ($this->authors as $authorData) {
-            $contributorNode = $dom->createElement('contrib');
-            $contributorNode->setAttribute('contrib-type', 'author');
-
-            $nameNode = $dom->createElement('name');
-            $givenNamesNode = $dom->createElement('given-names', $authorData['givenName']);
-            $surnameNode = $dom->createElement('surname', $authorData['familyName']);
-            $nameNode->appendChild($givenNamesNode);
-            $nameNode->appendChild($surnameNode);
-            $contributorNode->appendChild($nameNode);
-    
-            $emailNode = $dom->createElement('email', $authorData['email']);
-            $contributorNode->appendChild($emailNode);
-
-            if (isset($authorData['affiliation'])) {
-                $affiliations[] = $authorData['affiliation'];
-                $xrefNode = $dom->createElement('xref');
-                $xrefNode->setAttribute('ref-type', 'aff');
-                $xrefNode->setAttribute('rid', 'aff' . count($affiliations));
-                $contributorNode->appendChild($xrefNode);
-            }
-
-            $contributorGroupNode->appendChild($contributorNode);
-        }
-
-        foreach($affiliations as $index => $affiliation) {
-            $affNode = $dom->createElement('aff');
-            $affNode->setAttribute('id', 'aff' . ($index + 1));
-
-            $institutionNode = $dom->createElement('institution');
-            $institutionNode->appendChild($dom->createTextNode($affiliation));
-            $affNode->appendChild($institutionNode);
-            $contributorGroupNode->appendChild($affNode);
-        }
-
-        $articleMetaNode->appendChild($contributorGroupNode);
-
-        return $articleMetaNode;
-    }
 
     private function createSubmission()
     {
@@ -231,7 +118,8 @@ class MetadataXmlBuilderTest extends DatabaseTestCase
         $writtenXml = new DOMDocument();
         $writtenXml->load($this->xmlPath);
 
-        $expectedXml = $this->createExpectedXml();
+        $expectedXml = new DOMDocument();
+        $expectedXml->load(__DIR__ . '/fixtures/jats_metadata.xml');
 
         $this->assertEquals($expectedXml->saveXML(), $writtenXml->saveXML());
     }
