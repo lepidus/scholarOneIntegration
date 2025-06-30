8c82b7b997346c99f9c79361d38b562b0accde29
Jhon <jhon@lepidus.com.br>
2025-06-30 18:22:28 -0400

    Adds header node to Go XML file
    Issue: documentacao-e-tarefas/scielo#792
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/GoXmlBuilder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace APP\plugins\generic\scholarOneIntegration\classes;
+
+use DOMDocument;
+use APP\submission\Submission;
+use PKP\db\DAORegistry;
+
+class GoXmlBuilder
+{
+    private $clientKey;
+    private $journalShortName;
+
+    public function __construct(string $clientKey, string $journalShortName)
+    {
+        $this->clientKey = $clientKey;
+        $this->journalShortName = $journalShortName;
+    }
+
+    public function createGoXml(string $xmlFilePath): void
+    {
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+
+        $docType = $dom->implementation->createDocumentType('GO', '', 'S1_GO.dtd');
+        $dom->appendChild($docType);
+
+        $goNode = $dom->createElement('GO');
+        $dom->appendChild($goNode);
+
+        $headerNode = $dom->createElement('header');
+        $clientKeyNode = $dom->createElement('clientkey', $this->clientKey);
+        $headerNode->appendChild($clientKeyNode);
+        $journalAbbreviationNode = $dom->createElement('journal_abbreviation', $this->journalShortName);
+        $headerNode->appendChild($journalAbbreviationNode);
+        $goNode->appendChild($headerNode);
+
+        $dom->save($xmlFilePath);
+    }
+}
